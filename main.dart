5e5f68492342528395f2c4e8a16c6be6ef6e4bc9
bcs562793 <79099644+bcs562793@users.noreply.github.com>
2026-03-28 14:32:59 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -657,7 +657,7 @@ Future<void> _cleanStaleMatches() async {
     final res = await http.get(
       Uri.parse('$_sbUrl/rest/v1/live_matches'
           '?select=fixture_id,home_team,away_team,updated_at,status_short,score_source'
-          '&status_short=in.(1H,2H,HT,ET,BT,P,LIVE,NS)'),
+          '&status_short=in.(1H,2H,HT,ET,BT,P,LIVE)'),
       headers: _sbHeaders(),
     ).timeout(const Duration(seconds: 10));
     if (res.statusCode != 200) return;
